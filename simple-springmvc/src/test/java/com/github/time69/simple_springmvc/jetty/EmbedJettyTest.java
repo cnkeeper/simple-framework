49c203868b78ce054a15e2c85dbf796508fb2d41
TIME69 <1348555156@qq.com>
2018-08-22 11:51:44 +0800

    [test](module): refactor the jetty tests.
    Description:
       refactor the jetty tests.

--- simple-springmvc/src/test/java/com/github/time69/simple_springmvc/jetty/EmbedJettyTest.java
+++ simple-springmvc/src/test/java/com/github/time69/simple_springmvc/jetty/EmbedJettyTest.java
@@ -1,20 +1,19 @@
 package com.github.time69.simple_springmvc.jetty;
 
-import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.ErrorHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.servlet.Servlet;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 描述: 内嵌式jetty的使用测试
@@ -22,6 +21,7 @@ import java.io.IOException;
  * @author <a href="1348555156@qq.com">LeiLi.Zhang</a>
  * @version 0.0.0
  * @date 2018/8/21
+ * @see //www.eclipse.org/jetty/documentation/9.3.x/embedding-jetty.html
  */
 public class EmbedJettyTest {
     private Server server;
@@ -35,8 +35,18 @@ public class EmbedJettyTest {
     public void startJettyServer() {
 
         final int port = 8080;
-        server.addConnector(createServerConnector(server, port));
+        server.addConnector(JettyFactory.createServerConnector(server, port));
 
+        Map<Class<? extends Servlet>,String> servletMap = new HashMap<>();
+        servletMap.put(HelloServlet.class,"/hello");
+        HandlerList handlerList = new HandlerList();
+        handlerList.setHandlers(new Handler[]{
+                JettyFactory.createWebAppContext("/jetty","./webapp",servletMap),
+//                JettyFactory.createResourceHandler("./webapp/"),
+                new DefaultHandler()
+        });
+
+        server.setHandler(handlerList);
         // JVM退出时关闭Jetty
         server.setStopAtShutdown(true);
         try {
@@ -46,42 +56,6 @@ public class EmbedJettyTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * @return Connector
-     */
-    public ServerConnector createServerConnector(Server server, int port) {
-        ServerConnector httpServerConnector = new ServerConnector(server);
-        httpServerConnector.setHost("localhost");
-        httpServerConnector.setPort(port);
-        httpServerConnector.setIdleTimeout(30 * 1000);
-        return httpServerConnector;
-    }
-
-    /**
-     * 创建Servlet处理器
-     *
-     * @return Servlet处理器
-     */
-    public static ServletHandler createServletHandler() {
-        ServletHandler servletHandler = new ServletHandler();
-//        servletHandler.addServletWithMapping(createHuluServletHolder(), "/*");
-        return servletHandler;
-    }
-
-
-    /**
-     * 静态资源处理器
-     *
-     * @return 静态资源处理器
-     */
-    public static ResourceHandler createResourceHandler(String resourceBase) {
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setDirectoriesListed(true);
-        resourceHandler.setWelcomeFiles(new String[]{"index.html"});
-        resourceHandler.setResourceBase(resourceBase);
-        return resourceHandler;
     }
 
     public static void main(String[] args) throws Exception {
@@ -95,42 +69,11 @@ public class EmbedJettyTest {
         http.setIdleTimeout(30000);
         server.addConnector(http);
 
-        server.setHandler(new HelloWorldHandler());
+        server.setHandler(new HelloHandler());
         server.setErrorHandler(new ErrorHandler());
 
         server.start();
         server.dumpStdErr();
         server.join();
     }
-
-    private static class HelloWorldHandler extends AbstractHandler {
-
-        @Override
-        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-            // Declare response encoding and types
-            response.setContentType("text/html; charset=utf-8");
-
-            // Declare response status code
-            response.setStatus(HttpServletResponse.SC_OK);
-
-            // Write back response
-            response.getWriter().println("<h1>Jetty Hello World!</h1>");
-
-            // Inform jetty that this request has now been handled
-            baseRequest.setHandled(true);
-        }
-    }
-
-    @SuppressWarnings("serial")
-    public static class HelloServlet extends HttpServlet {
-        @Override
-        protected void doGet(HttpServletRequest request,
-                             HttpServletResponse response) throws ServletException,
-                IOException {
-            response.setContentType("text/html");
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<h1>Hello from HelloServlet</h1>");
-        }
-    }
-
 }
--- simple-springmvc/src/test/java/com/github/time69/simple_springmvc/tomcat/EmbedTomcatTest.java
+++ simple-springmvc/src/test/java/com/github/time69/simple_springmvc/tomcat/EmbedTomcatTest.java
@@ -3,7 +3,6 @@ package com.github.time69.simple_springmvc.tomcat;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.util.descriptor.web.ErrorPage;
 import org.apache.tomcat.util.descriptor.web.FilterDef;
 import org.apache.tomcat.util.descriptor.web.FilterMap;
 import org.junit.Before;
@@ -11,11 +10,7 @@ import org.junit.Test;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * 描述: 内嵌式tomcat的使用测试
@@ -25,114 +20,26 @@ import java.io.PrintWriter;
  * @date 2018/8/21
  */
 public class EmbedTomcatTest {
-    private Tomcat tomcat;
 
-    @Before
-    public void construct() {
-        tomcat = new Tomcat();
-    }
-
-
-    @Test
-    public void testEmbedTomcat() throws ServletException, LifecycleException {
-        tomcat.setBaseDir("tmp");
-        tomcat.setHostname("localhost");
-        tomcat.setPort(8080);
-
-        String contextPath = "/tomcat";
+    public static void main(String[] args) {
+        Tomcat tomcat = TomcatFactory.createTomcat(8080);
 //        String webappDir = new File("webapp").getAbsolutePath();
 //        tomcat.addWebapp(contextPath, webappDir);
-
+        String contextPath = "/tomcat";
         String docBase = new File(".").getAbsolutePath();
-        Context context = addContxt(contextPath, docBase);
-        addServlet(context);
+
+        Context context = TomcatFactory.createAndAddContext(tomcat, contextPath, docBase, null);
+
+        TomcatFactory.addFilter(context, "helloFilter", "/*", new HelloFilter());
+
+        TomcatFactory.addServlet(tomcat, context, "helloServlet", "/*", new HelloServlet());
 
         // curl http://localhost:8080/tomcat/hello/
-        start();
+        try {
+            TomcatFactory.start(tomcat);
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
     }
 
-
-    /**
-     * 启动服务
-     */
-    public void start() throws LifecycleException {
-        tomcat.start();
-        tomcat.getServer().await();
-    }
-
-
-    /**
-     * contextPath配置 <br/>
-     * 等效于配置conf/server.xml
-     * <pre>
-     *     <Context path="/" docBase="/ump-wwwroot" reloadable="true"></Context>
-     * </pre>
-     */
-    private Context addContxt(String contextPath, String docBase) {
-//        String contextPath = "/";
-//        String docBase = new File(".").getAbsolutePath();
-        Context context = tomcat.addContext(contextPath, docBase);
-
-        context.addErrorPage(new ErrorPage());
-        context.setCookies(true);
-        context.setSessionTimeout(30);
-
-        /**
-         * <pre>
-         * 等效于配置:
-         * <context-param>
-         *     <param-name>key</param-name>
-         *     <param-value>value</param-value>
-         * </context-param>
-         * </pre>
-         */
-        context.addParameter("key", "value");
-        return context;
-    }
-
-    /**
-     * servlet配置  <br/>
-     * 等效于在web.xml配置servlet, servlet-mapping节点
-     *
-     * @param context
-     */
-    private void addServlet(Context context) throws ServletException {
-        String servletName = "helloServlet";
-        String urlPattern = "/hello";
-        HttpServlet servlet = new HelloServlet();
-        tomcat.addServlet(context, servletName, servlet);
-        context.addServletMappingDecoded(urlPattern, servletName);
-    }
-
-    /**
-     * <pre>
-     *     &lt;filter&gt;
-     *         &lt;filter-name&gt;helloFilter&lt;/filter-name&gt;
-     *         &lt;filter-class&gt;***.HelloFilter&lt;/filter-class&gt;
-     *         &lt;inti-param&gt;
-     *             &lt;param—name&gt;key&lt;/param—name&gt;
-     *             &lt;param-value&gt;value&lt;/param-value&gt;
-     *         &lt;/inti-param&gt;
-     *     &lt;/filter&gt;
-     *     &lt;filter-mapping&gt;
-     *          &lt;filter-name&gt;helloFilter&lt;/filter-name&gt;
-     *          &lt;url-pattern&gt;/*&lt;/url-pattern>
-     *      &lt;/filter-mapping&gt;
-     * </pre>
-     */
-    private void addFilter(Context context) {
-        // filter
-        FilterDef filterDef = new FilterDef();
-        filterDef.addInitParameter("key", "value");
-
-        filterDef.setFilter(new HelloFilter());
-        filterDef.setFilterName("helloFilter");
-        context.addFilterDef(filterDef);
-
-        //filter-mapping
-        FilterMap filterMap = new FilterMap();
-        filterMap.addURLPattern("/*");
-        filterMap.setFilterName("helloFilter");
-        context.addFilterMap(filterMap);
-    }
 }
