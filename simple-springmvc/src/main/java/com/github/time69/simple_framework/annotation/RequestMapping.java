ee35e0f3025f07be215e10ea560733cd9af4bc11
TIME69 <1348555156@qq.com>
2018-08-23 19:04:47 +0800

    [feature](module): some annotations class.
    Description:
       some annotations class.

---
+++ simple-springmvc/src/main/java/com/github/time69/simple_framework/annotation/RequestMapping.java
@@ -0,0 +1,25 @@
+package com.github.time69.simple_framework.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 描述~
+ *
+ * @author <a href="1348555156@qq.com">LeiLi.Zhang</a>
+ * @version 0.0.0
+ * @date 2018/8/23
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RequestMapping {
+    /*路径*/
+    String path();
+
+    /*请求方法*/
+    Method[] method() default {Method.GET, Method.POST};
+
+    enum Method {
+        GET, POST, PUT, OPTION, DELETE;
+    }
+}
